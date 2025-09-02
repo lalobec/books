51713a4b668b61c91f935accab21d432f025b25a
lalobgc <lalobg.c@gmail.com>
2025-09-01 22:48:59 -0600

    add, complete, and delete functions created

---
+++ rggo/interacting/todo/todo.go
@@ -0,0 +1,53 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type item struct {
+	Task string
+	Done bool
+	CreatedAt time.Time
+	CompletedAt time.Time
+}
+
+type List []item
+
+func (l *List) Add(task string) {
+	t := item {
+		Task: task,
+		Done: false,
+		CreatedAt: time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*l = append(*l, t)
+}
+
+
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist in the task list", i)
+	}
+
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist in the task list", i)
+	}
+
+	*l = append(ls[:i-1], ls[i:]...)
+	return nil
+}
